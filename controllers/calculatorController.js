4513a867eba227f9c8e64c2531ae8973242af293
Carlos <carlosempd@gmail.com>
2023-07-12 16:11:20 -0400

    :sparkles: feat: create functionality for substraction division and multiplication

--- controllers/calculatorController.js
+++ controllers/calculatorController.js
@@ -1,6 +1,6 @@
 const twoDigitsOperationSchema = require("../validators/calculatorValidator")
 
-const sum = ((req, res) => {
+const addition = ((req, res) => {
     const { error, value } = twoDigitsOperationSchema.validate(req.body);
     
     if (error) {
@@ -14,6 +14,57 @@ const sum = ((req, res) => {
     })
 })
 
+const substraction = ((req, res) => {
+    const { error, value } = twoDigitsOperationSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({
+            message: error.message
+        })
+    }
+
+    res.status(200).json({
+        result: value.firstDigit - value.secondDigit
+    })
+})
+
+const multiplication = ((req, res) => {
+    const { error, value } = twoDigitsOperationSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({
+            message: error.message
+        })
+    }
+
+    res.status(200).json({
+        result: value.firstDigit * value.secondDigit
+    })
+})
+
+const division = ((req, res) => {
+    const { error, value } = twoDigitsOperationSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({
+            message: error.message
+        })
+    }
+
+    if (value.secondDigit === 0) {
+        return res.status(400).json({
+            message: 'Division by zero is undefined'
+        })
+    }
+
+    res.status(200).json({
+        result: value.firstDigit / value.secondDigit
+    })
+})
+
 module.exports = {
-    sum
+    addition,
+    substraction,
+    multiplication,
+    division
 }
--- routes/calculatorRoutes.js
+++ routes/calculatorRoutes.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const { sum } = require('../controllers/calculatorController')
+const { 
+    addition, 
+    substraction, 
+    multiplication, 
+    division } = require('../controllers/calculatorController')
 
-router.post('/sum', sum)
+router.post('/addition', addition)
+router.post('/substraction', substraction)
+router.post('/multiplication', multiplication)
+router.post('/division', division)
 
 module.exports = router
