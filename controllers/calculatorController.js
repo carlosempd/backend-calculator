9b6193c1809879c1fcfd2e4f07441a062c848b44
Carlos <carlosempd@gmail.com>
2023-07-12 16:24:49 -0400

    :sparkles: feat: upate operations to use two Digits middleware

--- controllers/calculatorController.js
+++ controllers/calculatorController.js
@@ -1,64 +1,36 @@
 const twoDigitsOperationSchema = require("../validators/calculatorValidator")
 
 const addition = ((req, res) => {
-    const { error, value } = twoDigitsOperationSchema.validate(req.body);
-    
-    if (error) {
-        return res.status(400).json({
-            message: error.message
-        })
-    }
 
     res.status(200).json({
-        result: value.firstDigit + value.secondDigit
+        result: req.value.firstDigit + req.value.secondDigit
     })
 })
 
 const substraction = ((req, res) => {
-    const { error, value } = twoDigitsOperationSchema.validate(req.body);
-
-    if (error) {
-        return res.status(400).json({
-            message: error.message
-        })
-    }
 
     res.status(200).json({
-        result: value.firstDigit - value.secondDigit
+        result: req.value.firstDigit - req.value.secondDigit
     })
 })
 
 const multiplication = ((req, res) => {
-    const { error, value } = twoDigitsOperationSchema.validate(req.body);
-
-    if (error) {
-        return res.status(400).json({
-            message: error.message
-        })
-    }
 
     res.status(200).json({
-        result: value.firstDigit * value.secondDigit
+        result: req.value.firstDigit * req.value.secondDigit
     })
 })
 
 const division = ((req, res) => {
-    const { error, value } = twoDigitsOperationSchema.validate(req.body);
 
-    if (error) {
-        return res.status(400).json({
-            message: error.message
-        })
-    }
-
-    if (value.secondDigit === 0) {
+    if (req.value.secondDigit === 0) {
         return res.status(400).json({
             message: 'Division by zero is undefined'
         })
     }
 
     res.status(200).json({
-        result: value.firstDigit / value.secondDigit
+        result: req.value.firstDigit / req.value.secondDigit
     })
 })
 
--- routes/calculatorRoutes.js
+++ routes/calculatorRoutes.js
@@ -5,10 +5,11 @@ const {
     substraction, 
     multiplication, 
     division } = require('../controllers/calculatorController')
+const twoDigitsOperationMiddleware = require('../middlewares/twoDigitsOperationMidlleware')
 
-router.post('/addition', addition)
-router.post('/substraction', substraction)
-router.post('/multiplication', multiplication)
-router.post('/division', division)
+router.post('/addition', twoDigitsOperationMiddleware, addition)
+router.post('/substraction', twoDigitsOperationMiddleware, substraction)
+router.post('/multiplication', twoDigitsOperationMiddleware, multiplication)
+router.post('/division', twoDigitsOperationMiddleware, division)
 
 module.exports = router
