2140cddcbc584d21034c3f983e2181d96bdb2f8a
Carlos <carlosempd@gmail.com>
2023-07-13 10:19:13 -0400

    :construction: feat: implement authentication middleware to generate string route

--- routes/calculatorRoutes.js
+++ routes/calculatorRoutes.js
@@ -9,12 +9,13 @@ const {
     randomString } = require('../controllers/calculatorController')
 const twoDigitsOperationMiddleware = require('../middlewares/twoDigitsOperationMidlleware')
 const oneDigitsOperationMiddleware = require('../middlewares/oneDigitOperationMiddleware')
+const { authenticate } = require('../middlewares/authMiddleware')
 
 router.post('/addition', twoDigitsOperationMiddleware, addition)
 router.post('/substraction', twoDigitsOperationMiddleware, substraction)
 router.post('/multiplication', twoDigitsOperationMiddleware, multiplication)
 router.post('/division', twoDigitsOperationMiddleware, division)
 router.post('/square-root', oneDigitsOperationMiddleware, squareRoot)
-router.get('/random-string', randomString)
+router.get('/random-string', authenticate, randomString)
 
 module.exports = router
