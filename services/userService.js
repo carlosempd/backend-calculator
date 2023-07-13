059745528f2165c56dbf2110dad86b56b98baa1d
Carlos <carlosempd@gmail.com>
2023-07-13 16:12:09 -0400

    :sparkles: feat: handle user credits to process operations

---
+++ services/userService.js
@@ -0,0 +1,21 @@
+const User = require('../models/user')
+
+const updateUserBalance = async(id, cost) => {
+    try {
+        const user = await User.findById(id);
+        const newBalance = user.balance - cost;
+        if (newBalance < 0) {
+            return -1
+        }
+        user.balance = newBalance
+        await user.save()
+        return newBalance;
+
+    } catch (error) {
+        throw new Error(error.message)
+    }
+}
+
+module.exports = {
+    updateUserBalance
+}
